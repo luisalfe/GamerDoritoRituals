4868dab7211fd56c759e53b2311911d7fb286ebd
luisalfe <luisalfe@myuax.com>
2024-12-20 11:49:56 +0100

    v2 Creado databasemanager y modelo

--- app/src/main/java/com/luis/gamerdoritorituals/MainActivity.java
+++ app/src/main/java/com/luis/gamerdoritorituals/MainActivity.java
@@ -1,6 +1,10 @@
 package com.luis.gamerdoritorituals;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +19,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // Ajustes para insets del sistema
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Zona para ir vinculando los elementos
+        Button btnAddTask = findViewById(R.id.btnAddTask);
+        ListView lvTasks = findViewById(R.id.lvTasks);
+
+        // Configuracion del boton para añadir tareas
+        btnAddTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Añade nuevo ritual de Gamer", Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
